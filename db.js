ed787723f6b279d4c3b2acfd04b9d5bc677bce0c
Mudit Gupta <muditgupta.rocks@gmail.com>
2019-08-11 16:47:31 +0530

    server
    Signed-off-by: Mudit Gupta <muditgupta.rocks@gmail.com>

---
+++ db.js
@@ -0,0 +1,24 @@
+const sequelize=require('sequelize')
+const db = new Sequelize('mydb', 'root', 'root', {
+    dialect: 'sqlite',
+    storage: 'feeds.db'
+  })
+  const Feeds=db.define('feeds',{
+      id:{
+          type:Sequelize.INTEGER,
+          autoIncrement:true,
+          primaryKey:true
+      },
+      content:{
+          allowNull:false,
+          type:Sequelize.STRING
+      }
+  })
+  db.sync()
+    .then(() => console.log("Database has been synced"))
+    .catch((err) => console.error("Error creating database"))
+
+exports = module.exports = {
+   Feeds
+}
+  
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/api', require('./api').route)
+
+app.listen(3300, () => console.log('Server started at http://localhost:3300'))
